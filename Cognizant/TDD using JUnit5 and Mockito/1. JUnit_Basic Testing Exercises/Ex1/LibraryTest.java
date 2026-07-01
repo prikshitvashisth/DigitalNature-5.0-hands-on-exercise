69c6c5caeadbd78a225f3c9e0c8e026286e5b276
Prikshit Vashisth <136953371+prikshitvashisth@users.noreply.github.com>
2026-07-01 23:10:58 +0530

    Add basic JUnit test for Library class

---
+++ Cognizant/TDD using JUnit5 and Mockito/1. JUnit_Basic Testing Exercises/Ex1/LibraryTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void testExample() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
